f62331855ca39bb9ac6a3260d4368852599ea371
Tareq <tareq_hassan3336610@hotmail.com>
2014-05-09 11:56:19 +0600

    First Commit

---
+++ constructors/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTestBox
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsNotNull(mybox);
+        }
+    }
+}
